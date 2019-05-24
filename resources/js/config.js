94da94eac4b54f1e27d3b957b8ce69bee8964692
Atunje <atunje_afolabi@yahoo.com>
2019-05-24 14:28:52 +0100

    Add API requests with javascript

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,3 +1,21 @@
 /**
  * Created by Atunje on 24/05/2019.
  */
+
+/*
+ Defines the API route we are using.
+ */
+var api_url = '';
+
+switch( process.env.NODE_ENV ){
+    case 'development':
+        api_url = 'https://roast-and-brew.app/api/v1';
+        break;
+    case 'production':
+        api_url = 'https://roastandbrew.coffee/api/v1';
+        break;
+}
+
+export const ROAST_CONFIG = {
+    API_URL: api_url,
+}
